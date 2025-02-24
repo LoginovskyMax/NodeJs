11aa2420e47b7423a048a60268582654d55e594c
Maxim Loginovskiy <frakiec3600@gmail.com>
2025-02-24 22:52:52 +0500

    fear:json

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,97 @@
-const btn = document.querySelector('#btn')
+const addBtn = document.querySelector('#add')
+const table = document.querySelector('#table')
+const modal = document.querySelector('#modal')
+// кнопки в модалке
+const closeBtn = document.querySelector('#modalClose')
+const createBtn = document.querySelector('#create')
 
-console.log('hi');
 
-btn.addEventListener('click', () => {
-    console.log('click');
+function getData(){
+    fetch('http://localhost:3003/students', {method: 'GET'})
+     .then((data) => data.json())
+     .then((data) => {
+        console.log(data);
+        drawPage(data)
+     })
+}
+
+function drawPage(arr){
+   arr.forEach(student => {
+      const div = document.createElement('div')
+      div.classList.add('table__item')
+      const name = document.createElement('p')
+      name.textContent = student.name
+      const gender = document.createElement('p')
+      gender.textContent = student.gender
+      const maths = document.createElement('p')
+      maths.textContent = student.maths
+      const physics = document.createElement('p')
+      physics.textContent = student.physics
+      const english = document.createElement('p')
+      english.textContent = student.english
+      const button = document.createElement('button')
+      button.textContent = 'Изменить'
+      div.append(name, gender, english, maths, physics,button)
+      table.append(div)
+   });
+}
+
+getData()
+
+let showModal = false
+
+addBtn.addEventListener('click', () => {
+    showModal = true
+    if(showModal){
+       modal.style.display = 'flex'
+    }else {
+       modal.style.display = 'none'
+    }
 })
+
+closeBtn.addEventListener('click', () => {
+    showModal = false
+    modal.style.display = 'none'
+})
+
+// инпуты формы
+const nameInp = document.querySelector('#name')
+const genderInp = document.querySelector('#gender')
+const englishInp = document.querySelector('#english')
+const physicsInp = document.querySelector('#physics')
+const mathInp = document.querySelector('#math')
+
+createBtn.addEventListener('click', () => {
+   const payload = {}
+   payload.name = nameInp.value
+   payload.gender = genderInp.value
+   payload.maths = englishInp.value
+   payload.physics = physicsInp.value
+   payload.english = mathInp.value
+
+   if(Object.values(payload).some(val => !val)){
+    console.log('Заполните все поля');
+    return
+   }
+   addStudent(payload)
+})
+function addStudent(payload) {
+
+    fetch('http://localhost:3003/students', {
+    method: 'POST',
+    headers: {
+       'content-type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+})
+    .then((data) => {
+        console.log(data);
+        if(data.ok){
+            console.log('Успешно создан');
+        }
+    })
+    .then((data) => {
+       data = 'success'
+    })
+}
 
--- src/express.ts
+++ src/express.ts
@@ -1,106 +1,62 @@
 import express from 'express'
-import {Request, Response} from 'express'
 const app = express()
+import fs from 'node:fs/promises'
+import path from 'node:path'
 const port = 3003
 // обрабатыавет боди
 const bodyJsonMiddleWare = express.json()
 const staticMid = express.static('./pages')
 app.use(bodyJsonMiddleWare)
 app.use(staticMid)
-app.use((req, res, next) => {
-  console.log('middleware work');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-})
 
-const users = [
-  {id:0, name: 'Max'},
-  {id:1, name: 'Alice'}
-]
-const cours = [
-  {id:0, name: 'front-end'},
-  {id:1, name: 'back-end'},
-  {id:1, name: 'devops'}
-]
-const db = {
-  users: users,
-  cours: cours
-}
 
-app.get('/',(req, res) => {
-  console.log('hi');
+
+app.get('/',async (req, res) => {
   res.sendFile('./home.html', {root: './pages' });
 })
 
-// app.get('/',(req, res) => {
-//   console.log('hi');
-//   res.sendFile('./home.html', {root: './pages' });
-// })
+function getPath(file:string){
+  const pathToFile = path.join(__dirname, file)
+  return pathToFile
+}
 
+async function getStudents(){
+  const jsonFile = await fs.readFile(getPath('db.json'), 'utf8')
+  return JSON.parse(jsonFile)
+}
 
-app.get('/cours', (req:Request<{title:string}>, res) => {
-  let filtered = db.cours
-  if(req.query.title) {
-    filtered = filtered.filter(cours => cours.name.indexOf(req.query.title as string) > -1)
-  }
-    res.json(filtered)
-  }) 
+async function setStudents(file:Object[]){
+  const jsonFile = await fs.writeFile(getPath('db.json'),JSON.stringify(file), 'utf8')
+}
 
-app.post('/cours', (req, res:Response<{id: number, name:string}[]>) => {
-  const cours = {
-    id: Date.now(),
-    ...req.body
-  }
-    db.cours.push(cours)
-    res
-    .status(201)
-    .json(db.cours)
-  })
-app.delete('/cours/:id', (req, res) => {
-    db.cours = db.cours.filter(user => user.id !== +req.params.id)
-    res
-    .json(db.cours)
-})
-
-app.put('/cours/:id', (req, res) => {
-  const user = db.users.find(user => user.id === +req.params.id)
-  if(!user){
-    res.sendStatus(404)
-    return
-  }
-  user.name = req.body.name
-  res
-  .status(204)
-  .json(user)
-}) 
-
-app.route('/users')
-  .get((req, res) => {
-    throw new Error('error inn users')
-    res.json(db.users)
-  })
-  .post((req, res) => {
-    res.json('Add a userr')
+app.route('/students')
+  .get(async (req, res) => {
+     const students = await getStudents()
+    res.json(students)
+   })
+  .post(async(req, res) => {
+    // получаем всех студентов из файла
+    const students = await getStudents()
+    req.body.id = Date.now()
+    students.push(req.body)
+    // записываем новый список студентов в файл
+    await setStudents(students)
+    res.sendStatus(201)
   })
   .put((req, res) => {
     res.json('Update the book')
   })
 
 
-app.get('/users/:id', (req:Request<{ id: string }>, res) => {
-    const user = db.users.find(user => user.id === +req.params.id)
-    if(!user){
-      res.sendStatus(404)
-      return
-    }
-      res.json(user)
+app.get('/users/:id', (req, res) => {
+    // const user = db.users.find(user => user.id === +req.params.id)
+    // if(!user){
+    //   res.sendStatus(404)
+    //   return
+    // }
+    //   res.json(user)
 })
 
-// Middleware для обработки ошибок (должно быть последним)
-app.use((err: Error, req: Request, res: Response) => {
-  console.error(err.stack); // Логируем ошибку
-  res.status(500).send('Something broke!'); // Отправляем ответ с кодом 500
-  });
 
 app.listen(port, () => {
     console.log(`Server work on port ${port}`);
